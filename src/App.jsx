1f3ee5a234e7a850a7914b5dbc3d60965589b884
Nur Mohammad Rashed <rashed@nordfincapital.com>
2023-03-31 01:43:42 +0200

    feat:Header ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
 import React from "react";
-import Header from './components/header/Header'
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+let theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "3rem",
+    },
+    h2: {
+      fontSize: "2.6rem",
+    },
+    h3: {
+      fontSize: "2.4rem",
+    },
+    h4: {
+      fontSize: "2rem",
+    },
+    h5: {
+      fontSize: "1.6rem",
+    },
+    h6: {
+      fontSize: "1.2rem",
+    },
+    
+  },
+});
+theme = responsiveFontSizes(theme);
 
 const App = () => {
   return (
     <>
-      <Header />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </ThemeProvider>
     </>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import './header.css';
+import "./header.css";
+import { Box, Typography } from "@mui/material";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Box className="container header__container">
+        <Typography variant="h6">Hello I am</Typography>
+        <Typography variant="h1">Nur Mohammad Rashed</Typography>
+        <Typography variant="h6" className="text-light">Front-end Developer</Typography>
+        <h5>Hello I am</h5>
+        <h1>Nur Mohammad Rashed</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+      </Box>
+    </header>
+  );
 };
 
 export default Header;
